a5e9f9cd5316ba2c08ed578238434cbda7dd5936
Alex-Sander Zapotõlok <alex.zapotolok@vikk.ee>
2025-02-13 13:33:48 +0200

    Initial commit

---
+++ OOP lõputest.py
@@ -0,0 +1,5 @@
+# Kalkulaator
+
+print(int("Sisesta arv1: "))
+print(int("Sisesta arv2: "))
+ arv1 *
---
+++ pangandus child class.py
@@ -0,0 +1,22 @@
+# Child Class
+class Bank(User):
+    def __init__(self, name, age, gender):
+        super().__init__(name, age, gender)
+        self.balance = 0
+
+    def deposit(self, amount):
+        self.amount = amount
+        self.balance = self.balance + self.amount
+        print("Account balance has been updated : €", self.balance)
+
+    def withdraw(self, amount):
+        self.amount = amount
+        if self.amount > self.balance:
+            print("Insufficient Euro | Balance Available : €", self.balance)
+        else:
+            self.balance = self.balance - self.amount
+            print("Account balance has been updated : €", self.balance)
+
+    def view_balance(self):
+        self.show_details()
+        print("Account balance: €", self.balance)
