9e84b7f1a14a9932cc7e88bbaafde38ae7df528b
alex.zapotolok <alex.zapotolok@vikk.ee>
2025-03-28 11:13:40 +0200

    12

--- Objektide animeerimine kontroll.py
+++ Objektide animeerimine kontroll.py
@@ -1,12 +1,12 @@
 import sys
-
 import pygame
+
 pygame.init()
 
 screen_width = 640
 screen_height = 480
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption('Formula 1 võidusõidu möng')
+pygame.display.set_caption('Formula 1 võidusõidu mäng')
 clock = pygame.time.Clock()
 
 taust = pygame.image.load("bg_rally.jpg")
@@ -14,7 +14,6 @@ punane_auto = pygame.image.load("f1_red.png")
 sinine_auto = pygame.transform.rotate(pygame.image.load("f1_blue.png"), 180)
 sinine_auto2 = pygame.transform.rotate(pygame.image.load("f1_blue.png"), 180)
 sinine_auto3 = pygame.transform.rotate(pygame.image.load("f1_blue.png"), 180)
-
 
 speed = 3
 skoor = 0
@@ -34,10 +33,20 @@ move_left = False
 move_right = False
 
 gameover = False
+
+def reset_game():
+    global player_x, player_y, sin_auto, sin_auto2, sin_auto3, skoor
+    player_x = 297.5
+    player_y = 391
+    sin_auto = 32
+    sin_auto2 = 147
+    sin_auto3 = 87
+    skoor = 0
+
 while not gameover:
-    # fps
+
     clock.tick(60)
-    # mängu sulgemine ristist
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameover = True
@@ -47,6 +56,8 @@ while not gameover:
                 move_left = True
             if event.key == pygame.K_RIGHT:
                 move_right = True
+            if event.key == pygame.K_r:
+                reset_game()
 
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT:
@@ -71,15 +82,15 @@ while not gameover:
         skoor += 1
         sin_auto3 = 0
 
-
-
-
-
-
-
     sin_auto += speed
     sin_auto2 += speed
     sin_auto3 += speed
+
+
+    if (player_x < 175 + sinine_auto.get_width() and player_x + punane_auto.get_width() > 175 and player_y < sin_auto + sinine_auto.get_height() and player_y + punane_auto.get_height() > sin_auto) or \
+       (player_x < 297.5 + sinine_auto2.get_width() and player_x + punane_auto.get_width() > 297.5 and player_y < sin_auto2 + sinine_auto2.get_height() and player_y + punane_auto.get_height() > sin_auto2) or \
+       (player_x < 415 + sinine_auto3.get_width() and player_x + punane_auto.get_width() > 415 and player_y < sin_auto3 + sinine_auto3.get_height() and player_y + punane_auto.get_height() > sin_auto3):
+        reset_game()
 
     screen.blit(taust, (0, 0))
     screen.blit(punane_auto, (player_x, player_y))
@@ -90,8 +101,6 @@ while not gameover:
     font = pygame.font.Font(None, 30)
     text = font.render("Formula 1 skoori punktid: " + str(skoor), True, [0, 0, 0])
     screen.blit(text, [200, 0])
-
-
 
     pygame.display.flip()
 
